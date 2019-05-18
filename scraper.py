3de0022b66fadc72f9cee6b10f26982c7be4a086
zegheim <justinarista@gmail.com>
2019-05-18 11:00:45 +0100

    successfully scraped all courses

--- scraper.py
+++ scraper.py
@@ -10,37 +10,54 @@ from fake_useragent import UserAgent
 PROXY_URL = 'https://www.sslproxies.org/'
 BASE_URL = 'http://www.drps.ed.ac.uk/18_19_Archive_at_01-09-2018/dpt/'
 
+ua = UserAgent()
+s = requests.Session()
 
 def main():
-    ua = UserAgent()
-    proxies = get_proxies(ua)
+    proxies = get_proxies()
+    proxy_idx = get_random_proxy(proxies)
+    proxy = proxies[proxy_idx]
+    proxy_dict = {
+        "https": "https://{ip}:{port}" \
+        .format(ip = proxy['ip'], port = proxy['port'])
+        }
 
+    bs = get_site_html(BASE_URL + 'cx_subindex.htm',
+                       proxies=proxy_dict)
+    subj_list = get_url_list(bs, 'cx_sb.+.htm')
     
     courses = set()
-    bs = get_site_html(BASE_URL + 'cx_subindex.htm')
-    subj_list = get_url_list(bs, 'cx_sb.+.htm')
-    for idx, subj in enumerate(subj_list):
-        bs = get_site_html(BASE_URL + subj['href'])
+
+    for idx, subj in enumerate(subj_list):       
+        bs = get_site_html(BASE_URL + subj['href'],
+                           proxies=proxy_dict)
         courses_list = get_url_list(bs, 'cx[a-z]{4}[0-9]{5}.htm')
+
         for course in courses_list:
             if course['href'] not in courses:
                 courses.add(course['href'])
+        
+        if (idx + 1) % 10 == 0:
+            proxy_idx = get_random_proxy(proxies)
+            proxy = proxies[proxy_idx]
+            proxy_dict = {
+                "https": "https://{ip}:{port}" \
+                .format(ip = proxy['ip'], port = proxy['port'])
+            }
+
     print(courses)
 
-def get_proxies(ua):
-    with requests.Session() as s:
-        s.header = {'user-agent': ua.random}
-        html = s.get(PROXY_URL).text
-        bs = get_site_html(html)
-        proxies_table = bs.find(id='proxylisttable')
 
-        for row in proxies_table.tbody.find_all('tr'):
-            row_contents = row.find_all('td')
-            proxies.append({
-                'ip': row_contents[0].string,
-                'port': row_contents[1].string
-            })
-    
+def get_proxies():
+    proxies = []
+    bs = get_site_html(PROXY_URL)
+    proxies_table = bs.find(id='proxylisttable')
+    for row in proxies_table.tbody.find_all('tr'):
+        row_contents = row.find_all('td')
+        proxies.append({
+            'ip': row_contents[0].string,
+            'port': row_contents[1].string
+        })
     return proxies
 
 
@@ -48,17 +65,17 @@ def get_random_proxy(proxies):
     return random.randint(0, len(proxies) - 1)
 
 
-def get_site_html(url):
+def get_site_html(url, **kwargs):
+    global s, ua
     print('Scraping from {}'.format(url))
-    with requests.Session() as s:
-        s.headers = {'user-agent': ua.random}
-        try:
-            html = s.get(url).text
-        except requests.exceptions.RequestException as e:
-            print(e)
-            sys.exit(1)
+    s.headers = {'user-agent': ua.random}
+    try:
+        html = s.get(url, **kwargs).text
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
 
-        return BeautifulSoup(html, 'lxml')
+    return BeautifulSoup(html, 'lxml')
 
 
 def get_url_list(bs, regexp, tag='a'):
