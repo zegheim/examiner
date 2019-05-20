0a553bedc24768754409bab007e5ad8a9e24633e
zegheim <justinarista@gmail.com>
2019-05-20 16:00:23 +0100

    successfully scraped all courses

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # examiner
-Visualising University of Edinburgh's examination timetable
+Visualising University of Edinburgh's examination timetable.
+
+Proxy rotation inspired by https://codelike.pro/create-a-crawler-with-rotating-ip-proxy-in-python/
--- scraper.py
+++ scraper.py
@@ -2,15 +2,12 @@ import random
 import re
 import requests
 import sys
-import time
 
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
 
 PROXY_URL = 'https://www.sslproxies.org/'
 BASE_URL = 'http://www.drps.ed.ac.uk/18_19_Archive_at_01-09-2018/dpt/'
 
-ua = UserAgent()
 s = requests.Session()
 
 def main():
@@ -24,28 +21,34 @@ def main():
 
     bs = get_site_html(BASE_URL + 'cx_subindex.htm',
                        proxies=proxy_dict)
-    subj_list = get_url_list(bs, 'cx_sb.+.htm')
-    
-    courses = set()
+    schools = bs.find('table', {'class': 'content'}).find_all('h2')
 
-    for idx, subj in enumerate(subj_list):       
-        bs = get_site_html(BASE_URL + subj['href'],
-                           proxies=proxy_dict)
-        courses_list = get_url_list(bs, 'cx[a-z]{4}[0-9]{5}.htm')
+    with open('csv/courses.csv', 'w+') as f:
+        f.write('schedule, course_code, course_name, period, credits\n')
+        for school in schools:
+            schedule = school.text[-2]
+            subjects = get_url_list(school.next_sibling, 'cx_sb_[a-z]{4}.htm')
+            for idx, subject in enumerate(subjects):
+                bs = get_site_html(BASE_URL + subject['href'], proxies=proxy_dict)
+                courses = get_url_list(bs, 'cx[a-z]{4}[0-9]{5}.htm')
+                for course in courses:
+                    if course.has_attr('class') is not True:
+                        course_code = course.parent.find_previous_sibling('td', string=re.compile('[A-Z]{4}[0-9]{5}'))
+                        period = course.parent.next_sibling
+                        credit = period.next_sibling
+                        f.write('{}, {}, {}, {}, {}\n'
+                                .format(schedule, course_code.get_text(),
+                                        course.get_text(), period.get_text(), 
+                                        credit.get_text()))
 
-        for course in courses_list:
-            if course['href'] not in courses:
-                courses.add(course['href'])
-        
-        if (idx + 1) % 10 == 0:
-            proxy_idx = get_random_proxy(proxies)
-            proxy = proxies[proxy_idx]
-            proxy_dict = {
-                "https": "https://{ip}:{port}" \
-                .format(ip = proxy['ip'], port = proxy['port'])
-            }
-
-    print(courses)
+                # Rotate proxies used after 10 requests
+                if (idx + 1) % 10 == 0:
+                    proxy_idx = get_random_proxy(proxies)
+                    proxy = proxies[proxy_idx]
+                    proxy_dict = {
+                        "https": "https://{ip}:{port}" \
+                        .format(ip = proxy['ip'], port = proxy['port'])
+                }
 
 
 def get_proxies():
@@ -66,11 +69,10 @@ def get_random_proxy(proxies):
 
 
 def get_site_html(url, **kwargs):
-    global s, ua
-    print('Scraping from {}'.format(url))
-    s.headers = {'user-agent': ua.random}
+    global s
+    s.header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     try:
-        html = s.get(url, **kwargs).text
+        html = strip_wsnl(s.get(url, **kwargs).text)
     except requests.exceptions.RequestException as e:
         print(e)
         sys.exit(1)
@@ -80,6 +82,17 @@ def get_site_html(url, **kwargs):
 
 def get_url_list(bs, regexp, tag='a'):
     return bs.find_all(tag, {'href': re.compile(regexp)})
+
+
+def strip_wsnl(html):
+    """remove whitespaces and newlines"""
+    pat = re.compile('(^[\s]+)|([\s]+$)', re.MULTILINE)
+    html = re.sub(pat, '', html)        # remove leading and trailing whitespaces
+    html = re.sub('\n', ' ', html)      # convert newlines to spaces
+                                        # this preserves newline delimiters
+    html = re.sub('[\s]+<', '<', html)  # remove whitespaces before opening tags
+    html = re.sub('>[\s]+', '>', html)  # remove whitespaces after closing tags
+    return html
 
 if __name__ == '__main__':
     main()
